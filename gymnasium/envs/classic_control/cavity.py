c833475f0d0ad66f8910963dc504e63f1e49ac23
Andrea Svizzeretto <andreasvizzeretto@studenti.unipg.it>
2024-03-12 11:36:07 +0100

    Added files, like utils.py, spaces folder, ecc..  for correct import of packages inside cavity.py

--- gymnasium/envs/classic_control/cavity.py
+++ gymnasium/envs/classic_control/cavity.py
@@ -11,8 +11,8 @@ import gymnasium as gym
 from gymnasium import logger, spaces
 from gymnasium.envs.classic_control import utils
 from gymnasium.error import DependencyNotInstalled
-from gymnasium.vector import VectorEnv
-from gymnasium.vector.utils import batch_space
+#from gymnasium.vector import VectorEnv
+#from gymnasium.vector.utils import batch_space
 
 import oreonspy as op
 
