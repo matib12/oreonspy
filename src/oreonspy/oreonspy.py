1fb93cdd95290b03c952a02d01705e7ba89acb5b
Mateusz Bawaj <mateusz.bawaj@pg.infn.it>
2024-04-25 00:02:23 +0200

    Bumped oreonspy package version to v2.1.1.

--- src/oreonspy/oreonspy.py
+++ src/oreonspy/oreonspy.py
@@ -15,7 +15,7 @@ __email__ =  "mateusz.bawaj@unipg.it"
 __license__ = "GPLv3"
 __maintainer__ = "developer"
 __status__ = "Production"
-__version__ = '2.0.1'
+__version__ = '2.1.1'
 
 
 from scipy import constants as const
