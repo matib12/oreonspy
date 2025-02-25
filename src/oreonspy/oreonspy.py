c946e56015a5cb8f828664f1f77ad5b0e2826743
Mateusz Bawaj <mateusz.bawaj@pg.infn.it>
2025-02-25 12:03:10 +0100

    Bump version to 3.1.0 in pyproject.toml and update __version__ in oreonspy.py

--- src/oreonspy/oreonspy.py
+++ src/oreonspy/oreonspy.py
@@ -15,7 +15,7 @@ __email__ =  "mateusz.bawaj@unipg.it"
 __license__ = "GPLv3"
 __maintainer__ = "developer"
 __status__ = "Production"
-__version__ = '3.0.0'
+__version__ = '3.1.0'
 
 
 from scipy import constants as const
