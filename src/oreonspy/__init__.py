d9d9cb1a582518abf7deb1289bd89305b30f6555
Mateusz Bawaj <matusz.bawaj@unipg.it>
2026-04-04 00:53:53 +0200

    Bump version to 4.4.0 in pyproject.toml and __init__.py

--- src/oreonspy/__init__.py
+++ src/oreonspy/__init__.py
@@ -7,7 +7,7 @@ __email__ =  "mateusz.bawaj@unipg.it"
 __license__ = "GPLv3"
 __maintainer__ = "developer"
 __status__ = "Production"
-__version__ = '4.3.0'
+__version__ = '4.4.0'
 
 from .oreonspy import Cavity
 from .oreonspy import TestCavity, ArmCavity, FilterCavity
